dd8bec07d9d1bcb62ec24135bfa2e184ccd9bf1e
Kris Stern <kasternhk@gmail.com>
2021-04-07 06:07:02 +0800

    Create downloader.py

---
+++ edgar_fetch/downloader/downloader.py
@@ -0,0 +1,10 @@
+EDGAR_PREFIX = "https://www.sec.gov/Archives/"
+SEP = "|"
+
+class Fetcher():
+    def __init__(self):
+        pass
+
+    # There will be only accessors and no mutators
+    def get():
+        pass
